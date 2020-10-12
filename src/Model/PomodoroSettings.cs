ee2e1bd530cfc597e0c1ba6c26cb8039e2bdb987
Igor Anjos <igor.anjos@orbium.com.br>
2020-10-12 20:24:55 -0300

    Criação de uma aplicacao demo com blazor

---
+++ src/Model/PomodoroSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace demo_blazor_pomodoro.Model
+{
+    public class PomodoroSettings
+    {
+        [Required(ErrorMessage = "Especifica o tempo de duração do Pomodoro")]
+        [Range(1, 60, ErrorMessage = "O valor do tempo precisar ser entre 1 e 60.")]
+        public int PomodoroDuration { get; set; }
+
+        [Range(1, 15, ErrorMessage = "O valor do tempo precisa ser entre 1 e 15.")]
+        public int ShortBreak { get; set; }
+
+        [Range(15, 60, ErrorMessage = "O valor do tempo precisa ser entre 15 e 60.")]
+
+        public int LongBreak { get; set; }
+    }
+
+    public enum TimerType
+    {
+        Pomodoro = 1,
+        ShortBreak = 2,
+        LongBreak = 3
+    }
+}
